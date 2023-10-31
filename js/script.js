bcab15e6c577518db7f479234dad117eaeede55d
Roman - <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>
2023-10-31 04:56:45 +0100

    Fade in classes added

--- js/script.js
+++ js/script.js
@@ -33,3 +33,41 @@ document.addEventListener("DOMContentLoaded", () => {
   updateText();
   setInterval(updateText, 2500);
 });
+
+// CTAs
+const toAbout = document.querySelector(".to-about-cta");
+const toAboutDiv = document.querySelector(".about");
+
+toAbout.addEventListener("click", () => {
+  toAboutDiv.scrollIntoView({ behavior: "smooth" }); // Use "smooth" for smooth scrolling
+});
+
+//Intersection Observer
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to handle the fading effect
+function handleScroll() {
+  const elements = document.querySelectorAll(".cls");
+  elements.forEach((element) => {
+    if (isElementInViewport(element)) {
+      element.style.opacity = 1;
+    } else {
+      element.style.opacity = 0;
+    }
+  });
+}
+
+// Add an event listener to check for changes in the viewport
+window.addEventListener("scroll", handleScroll);
+
+// Initial check on page load
+handleScroll();
