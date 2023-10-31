e0c996d2ce101f98304921d725177a3f7edd0f20
Roman - <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>
2023-10-31 02:46:14 +0100

    About created

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
     "Dynamic",
     "Scalable",
     "Responsive",
-    "Interactive",
     "Accessible",
+    "Interactive",
     "Performant",
   ];
 
-  const bgArray = [
+  const textBgArray = [
     "linear-gradient(90deg, rgb(91, 204, 212), rgb(68, 106, 211))",
     "linear-gradient(90deg,rgb(184, 187, 35), rgb(225, 45, 231))",
     "linear-gradient(90deg,rgb(13, 173, 236),rgb(135, 202, 199))",
-    "linear-gradient(90deg, rgb(223, 88, 25), rgb(185, 147, 42))",
     "linear-gradient(90deg, rgb(24, 194, 33), rgb(37, 156, 101))",
+    "linear-gradient(90deg, rgb(223, 88, 25), rgb(185, 147, 42))",
     "linear-gradient(90deg, rgb(228, 207, 26), rgb(219, 94, 36))",
   ];
 
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function updateText() {
     effectText.classList.remove("dropDown");
     setTimeout(() => {
-      effectText.style.backgroundImage = bgArray[count];
+      effectText.style.backgroundImage = textBgArray[count];
       effectText.textContent = textArray[count];
       effectText.classList.add("dropDown");
       count = (count + 1) % textArray.length;
