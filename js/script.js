8e09967e49fee2f849dd6a87d0905adf10984d0b
Roman - <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>
2023-11-01 23:49:10 +0100

    Further attempt to improve performance

--- js/script.js
+++ js/script.js
@@ -1,50 +1,38 @@
-function isElementInViewport(el) {
-  var rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const effectText = document.querySelector(".effectTxt");
+  const textArray = [
+    "Dynamic",
+    "Scalable",
+    "Responsive",
+    "Accessible",
+    "Interactive",
+    "Performant",
+  ];
 
-// document.addEventListener("DOMContentLoaded", () => {
-const effectText = document.querySelector(".effectTxt");
-const textArray = [
-  "Dynamic",
-  "Scalable",
-  "Responsive",
-  "Accessible",
-  "Interactive",
-  "Performant",
-];
+  const textBgArray = [
+    "linear-gradient(90deg, rgb(91, 204, 212), rgb(68, 106, 211))",
+    "linear-gradient(90deg,rgb(184, 187, 35), rgb(225, 45, 231))",
+    "linear-gradient(90deg,rgb(13, 173, 236),rgb(135, 202, 199))",
+    "linear-gradient(90deg, rgb(24, 194, 33), rgb(37, 156, 101))",
+    "linear-gradient(90deg, rgb(223, 88, 25), rgb(185, 147, 42))",
+    "linear-gradient(90deg, rgb(228, 207, 26), rgb(219, 94, 36))",
+  ];
 
-const textBgArray = [
-  "linear-gradient(90deg, rgb(91, 204, 212), rgb(68, 106, 211))",
-  "linear-gradient(90deg,rgb(184, 187, 35), rgb(225, 45, 231))",
-  "linear-gradient(90deg,rgb(13, 173, 236),rgb(135, 202, 199))",
-  "linear-gradient(90deg, rgb(24, 194, 33), rgb(37, 156, 101))",
-  "linear-gradient(90deg, rgb(223, 88, 25), rgb(185, 147, 42))",
-  "linear-gradient(90deg, rgb(228, 207, 26), rgb(219, 94, 36))",
-];
+  let count = 0;
 
-let count = 0;
+  function updateText() {
+    effectText.classList.remove("dropDown");
+    setTimeout(() => {
+      effectText.style.backgroundImage = textBgArray[count];
+      effectText.textContent = textArray[count];
+      effectText.classList.add("dropDown");
+      count = (count + 1) % textArray.length;
+    }, 150);
+  }
 
-function updateText() {
-  effectText.classList.remove("dropDown");
-  setTimeout(() => {
-    effectText.style.backgroundImage = textBgArray[count];
-    effectText.textContent = textArray[count];
-    effectText.classList.add("dropDown");
-
-    count = (count + 1) % textArray.length;
-  }, 150);
-}
-
-updateText();
-setInterval(updateText, 2500);
-// });
+  updateText();
+  setInterval(updateText, 2500);
+});
 
 // CTAs
 const toAbout = document.querySelector(".to-about-cta");
@@ -63,26 +51,6 @@ toSolutions.addEventListener("click", () => {
 });
 
 //Intersection Observer
-
-function handleScroll() {
-  const elements = document.querySelectorAll(".cls");
-  const slideelements = document.querySelector(".cli");
-  elements.forEach((element) => {
-    if (isElementInViewport(element)) {
-      element.style.opacity = 1;
-    }
-  });
-
-  if (isElementInViewport(slideelements)) {
-    slideelements.classList.add("slideIn");
-  } else {
-    slideelements.classList.remove("slideIn");
-  }
-}
-
-window.addEventListener("scroll", handleScroll);
-
-// handleScroll();
 
 //Submit form
 
@@ -189,9 +157,7 @@ contactspan.addEventListener("click", () => {
 });
 
 //Scroll toggle nav
-
 const divs = Array.from(document.querySelectorAll(".container"));
-
 const navSpans = Array.from(document.querySelectorAll(".navspan"));
 
 window.onscroll = () => {
